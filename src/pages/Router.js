c7509b47e1acf68b5cf259fad1b85cb8910fa880
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-10 16:10:45 +0200

    Created router and layout

---
+++ src/pages/Router.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './Home'
+import FicheLogement from './FicheLogement'
+import APropos from './APropos'
+import Error from './Error'
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route index element={<Home />} />
+
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/fiche-logement" element={<FicheLogement />} />
+                    <Route path="/a-propos" element={<APropos />} />
+
+                    <Route path="*" element={<Error />} />
+                </Route>
+            </Routes>
+        </BrowserRouter >
+    );
+};
+
+export default Router;
