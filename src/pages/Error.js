8cc44e39a551a092856ec7349cf9e257b3dd743a
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-11 14:39:30 +0200

    Finished error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Error = () => {
     return (
         <div>
-
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <p><Link to='/home'>Retourner sue la page d'accueil</Link></p>
         </div>
     );
 };
