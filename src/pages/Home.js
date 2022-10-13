7bdc2a01fe152327f16dde8abc07e74126694417
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-13 14:31:59 +0200

    Finished home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Banner from '../components/Banner'
+import Card from '../components/Card'
+import { logementService } from '../_services/Logement.service'
+
+
 
 const Home = () => {
+
+    const [logements, setLogements] = useState([])
+    const flag = useRef(false)
+
+    useEffect(() => {
+
+        if (flag.current === false) {
+            logementService.getAllLogements()
+                .then(res => {
+                    setLogements(res)
+                })
+                .catch((err) => console.log(err))
+        }
+        return () => flag.current = true
+    }, [])
+
     return (
-        <div>
+        <>
             <Banner />
-            <p>Chez vous, partout et ailleurs</p>
-        </div>
+            <div className='cardsContainer'>
+                {
+                    logements.map(logement => (
+                        <Card logement={logement} key={logement.id} />
+                    ))
+                }
+            </div>
+        </>
     );
 };
 
