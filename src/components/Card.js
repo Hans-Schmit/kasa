db280ad2ce3804be60f050ac3fa3d243cab8f030
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-13 14:31:04 +0200

    Finished card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-const Card = () => {
+
+const Card = ({ logement }) => {
+
     return (
-        <div>
-
-        </div>
+        <div className='card'>
+            <Link to={`./fiche-logement/${logement.id}`}>
+                <img src={logement.cover} alt={logement.title}></img>
+                <p>{logement.title}</p>
+            </Link>
+        </div >
     );
 };
 
