944a1811876e026f4ca289d6a308a779d0aecdda
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-13 16:38:50 +0200

    Added recovery of datas and redirection

--- src/pages/FicheLogement.js
+++ src/pages/FicheLogement.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Navigate } from 'react-router-dom'
+import { logementService } from '../_services/Logement.service'
 
 const FicheLogement = () => {
-    return (
-        <>
 
+    const [logement, setLogement] = useState({})
+    const flagLoge = useRef(false)
+
+    useEffect(() => {
+        if (flagLoge.current === false) {
+            let logementId = new URLSearchParams(window.location.search).get("id")
+            logementService.getOneLogement(logementId)
+                .then(res => {
+                    setLogement(res)
+                })
+                .catch((error) => console.log(error))
+        }
+
+        return () => flagLoge.current = true
+    }, [])
+
+    if (logement === undefined) {
+        console.log('ok')
+        return <Navigate to="/error-404" replace={true} />
+    }
+
+    return (
+
+
+        <>
+            <p>fiche logement n°{logement.id}</p>
         </>
     );
 };
