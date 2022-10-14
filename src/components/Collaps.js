10d18237ab8a2d4ca109bfd9faf859484667fe92
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-14 14:52:04 +0200

    Almost finished Collaps (WIP css and arrow change)

--- src/components/Collaps.js
+++ src/components/Collaps.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Arrow from '../images/arrow.png'
 
-const Collaps = () => {
+const Collaps = ({ data }) => {
+
+    const [isActive, setIsActive] = useState(false)
+
     return (
-        <div>
+        <div className='collaps'>
+            <div className='collaps_title' onClick={() => setIsActive(!isActive)} style={{ backgroundColor: "#EE8888", color: "#FFFFFF" }}>
+                {data.title}
+                {isActive ?
+                    <img src={Arrow} alt='' ></img> :
+                    <img src={Arrow} alt='' style={{ transform: "rotate(180deg)" }}></img>
+                }
+            </div>
+
+            {isActive &&
+                <div className='collaps_content' style={{ backgroundColor: "#DEDEDE", color: "#EE8888" }}>
+                    {data.text}
+                </div>
+            }
+
 
         </div>
     );
