00e7cd4651ae4333a37ecb1ee816720da8f040a6
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-14 16:42:22 +0200

    Added components in the page

--- src/pages/FicheLogement.js
+++ src/pages/FicheLogement.js
@@ -1,10 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Navigate } from 'react-router-dom'
+import Carousel from '../components/Carousel'
+import Collaps from '../components/Collaps'
+import Tag from '../components/Tag'
+import Host from '../components/Host'
+import Rating from '../components/Rating'
 import { logementService } from '../_services/Logement.service'
 
 const FicheLogement = () => {
 
-    const [logement, setLogement] = useState({})
+    const [logement, setLogement] = useState({
+        "id": "",
+        "title": "",
+        "cover": "",
+        "pictures": [],
+        "description": "",
+        "host": {
+            "name": "",
+            "picture": ""
+        },
+        "rating": "",
+        "location": "",
+        "equipments": [],
+        "tags": []
+    })
     const flagLoge = useRef(false)
 
     useEffect(() => {
@@ -29,7 +48,20 @@ const FicheLogement = () => {
 
 
         <>
-            <p>fiche logement n°{logement.id}</p>
+            <Carousel data={logement.pictures} />
+            <p>{logement.title}</p>
+            <p>{logement.location}</p>
+            <div className='tagsContainer'>
+                {
+                    logement.tags.map(tag => (
+                        <Tag data={tag} key={tag} />
+                    ))
+                }
+            </div>
+            <Host data={logement.host} />
+            <Rating data={logement.rating} />
+            <Collaps data={{ title: "Description", text: logement.description }} />
+            <Collaps data={{ title: "Équipements", text: logement.equipments }} />
         </>
     );
 };
