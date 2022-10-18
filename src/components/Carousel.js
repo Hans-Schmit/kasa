b158fd322ea5f390724d36c16a816ebd29faa97d
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-18 15:22:22 +0200

    Finished Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowBack from '../images/arrow_back.png'
 import ArrowForward from '../images/arrow_forward.png'
 
+
 const Carousel = (pictures) => {
-    console.log(pictures);
+
+    const [imgNum, setImgNum] = useState(0)
+
+    const changeImg = (direction) => {
+        if (direction === 1) {
+            imgNum === pictures.data.length - 1 ? setImgNum(0) : setImgNum(imgNum + 1)
+        }
+        if (direction === -1) {
+            imgNum === 0 ? setImgNum(pictures.data.length - 1) : setImgNum(imgNum - 1)
+        }
+    }
+
     return (
         <div className='carrousel'>
-            <button id='backButton'><img src={ArrowBack} alt='' /></button>
-            {pictures.data.map(pictureUrl => (
-                <img src={pictureUrl} alt='' key={pictureUrl} />
-            ))}
-            <button id='forwardButton'><img src={ArrowForward} alt='' /></button>
+
+            <img src={pictures.data[imgNum]} alt={`Vue n° ${imgNum + 1}`} />
+
+            {(pictures.data.length > 1) &&
+                <>
+                    <button id='backButton' onClick={() => changeImg(-1)}><img src={ArrowBack} alt='flèche gauche' /></button>
+                    <span id="imgCounter">{imgNum + 1}/{pictures.data.length}</span>
+                    <button id='forwardButton' onClick={() => changeImg(1)}><img src={ArrowForward} alt='flèche droite' /></button>
+                </>
+            }
         </div>
     );
 };
