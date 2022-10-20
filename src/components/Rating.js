b15b86046d750843fce2388a583893fe343c219a
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-20 12:35:00 +0200

    Finished rating css

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import styles from './rating.module.css'
 import Redstar from '../images/red_star.png'
 import Whitestar from '../images/white_star.png'
 
 const Rating = (rate) => {
 
     return (
-        <div className='rating'>
-            {rate.data > 0 ? <img src={Redstar} alt='redstar' /> : <img src={Whitestar} alt='whitestar' />}
-            {rate.data > 1 ? <img src={Redstar} alt='redstar' /> : <img src={Whitestar} alt='whitestar' />}
-            {rate.data > 2 ? <img src={Redstar} alt='redstar' /> : <img src={Whitestar} alt='whitestar' />}
-            {rate.data > 3 ? <img src={Redstar} alt='redstar' /> : <img src={Whitestar} alt='whitestar' />}
-            {rate.data > 4 ? <img src={Redstar} alt='redstar' /> : <img src={Whitestar} alt='whitestar' />}
+        <div className={styles.rating}>
+            {rate.data > 0 ? <img src={Redstar} alt='redstar' className={styles.star} /> : <img src={Whitestar} alt='whitestar' className={styles.star} />}
+            {rate.data > 1 ? <img src={Redstar} alt='redstar' className={styles.star} /> : <img src={Whitestar} alt='whitestar' className={styles.star} />}
+            {rate.data > 2 ? <img src={Redstar} alt='redstar' className={styles.star} /> : <img src={Whitestar} alt='whitestar' className={styles.star} />}
+            {rate.data > 3 ? <img src={Redstar} alt='redstar' className={styles.star} /> : <img src={Whitestar} alt='whitestar' className={styles.star} />}
+            {rate.data > 4 ? <img src={Redstar} alt='redstar' className={styles.star} /> : <img src={Whitestar} alt='whitestar' className={styles.star} />}
         </div>
     );
 };
