6dfd1f14d316399a1f4f3d9a873b475c1fa1c0ec
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-20 12:06:38 +0200

    Finished card css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styles from './card.module.css'
 
 
 const Card = ({ logement }) => {
 
     return (
-        <div className='card'>
+        <div className={styles.card}>
             <Link to={`/fiche-logement?id=${logement.id}`}>
-                <img src={logement.cover} alt={logement.title}></img>
-                <p>{logement.title}</p>
+                <img src={logement.cover} alt={logement.title} className={styles.image}></img>
+                <p className={styles.title}>{logement.title}</p>
             </Link>
         </div >
     );
