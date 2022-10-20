a3def4b8bebdd143983ed57530cd35e49389bfde
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-20 15:49:49 +0200

    Started collaps css

--- src/components/Collaps.js
+++ src/components/Collaps.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styles from './collaps.module.css'
 import ArrowDown from '../images/arrow_down.png'
 import ArrowUp from '../images/arrow_up.png'
 
@@ -7,21 +8,21 @@ const Collaps = ({ data }) => {
     const [isActive, setIsActive] = useState(false)
 
     return (
-        <div className='collaps'>
-            <div className='collaps_title' onClick={() => setIsActive(!isActive)} style={{ backgroundColor: "#EE8888", color: "#FFFFFF" }}>
-                {data.title}
-                {isActive ? <img src={ArrowDown} alt='arrow down' ></img> : <img src={ArrowUp} alt='arrow up' ></img>}
+        <div className={styles.collaps}>
+            <div className={styles.title} onClick={() => setIsActive(!isActive)} >
+                <h2 className={styles.h2}>{data.title}</h2>
+                {isActive ? <img src={ArrowDown} alt='arrow down' className={styles.arrow}></img> : <img src={ArrowUp} alt='arrow up' className={styles.arrow}></img>}
             </div>
 
             {isActive &&
-                <div className='collaps_content' style={{ backgroundColor: "#DEDEDE", color: "#EE8888" }}>
+                <div className={styles.content} >
                     {(data.title === "Équipements" ?
-                        <ul>
+                        <ul className={styles.ul}>
                             {data.text.map(equipment => (
-                                <li key={equipment}>{equipment}</li>
+                                <li key={equipment} className={styles.li}>{equipment}</li>
                             ))}
                         </ul>
-                        : <p>{data.text}</p>)}
+                        : <p className={styles.text}>{data.text}</p>)}
                 </div>
             }
         </div>
