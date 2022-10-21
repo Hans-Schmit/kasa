f390f529e02195f1b22456a52ad73185f58521de
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-21 16:55:08 +0200

    Advanced collaps css

--- src/components/Collaps.js
+++ src/components/Collaps.js
@@ -8,21 +8,21 @@ const Collaps = ({ data }) => {
     const [isActive, setIsActive] = useState(false)
 
     return (
-        <div className={styles.collaps}>
-            <div className={styles.title} onClick={() => setIsActive(!isActive)} >
-                <h2 className={styles.h2}>{data.title}</h2>
-                {isActive ? <img src={ArrowDown} alt='arrow down' className={styles.arrow}></img> : <img src={ArrowUp} alt='arrow up' className={styles.arrow}></img>}
+        <div className={data.style === "Logement" ? styles.collapsLogement : styles.collaps}>
+            <div className={data.style === "Logement" ? styles.titleLogement : styles.title} onClick={() => setIsActive(!isActive)} >
+                <h2 className={data.style === "Logement" ? styles.h2Logement : styles.h2}>{data.title}</h2>
+                {isActive ? <img src={ArrowDown} alt='arrow down' className={data.style === "Logement" ? styles.arrowLogement : styles.arrow}></img> : <img src={ArrowUp} alt='arrow up' className={styles.arrow}></img>}
             </div>
 
             {isActive &&
-                <div className={styles.content} >
+                <div className={data.style === "Logement" ? styles.contentLogement : styles.content} >
                     {(data.title === "Équipements" ?
                         <ul className={styles.ul}>
                             {data.text.map(equipment => (
                                 <li key={equipment} className={styles.li}>{equipment}</li>
                             ))}
                         </ul>
-                        : <p className={styles.text}>{data.text}</p>)}
+                        : <p className={data.style === "Logement" ? styles.textLogement : styles.text}>{data.text}</p>)}
                 </div>
             }
         </div>
