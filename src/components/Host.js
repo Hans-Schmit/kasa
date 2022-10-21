654ea24b591c600199e50bfc099bfbf1dd923e04
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-21 15:27:23 +0200

    Finished host css

--- src/components/Host.js
+++ src/components/Host.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './host.module.css'
 
 const Host = (host) => {
     return (
-        <div className='host'>
-            <p>{host.data.name}</p>
-            <img src={host.data.picture} alt="host's face" />
+        <div className={styles.host}>
+            <p className={styles.text}>{host.data.name.split(' ')[0]}<br />{host.data.name.split(' ')[1]}</p>
+            <img src={host.data.picture} alt="host's face" className={styles.image} />
         </div>
     );
 };
