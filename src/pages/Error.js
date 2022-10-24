ea148b3901f94aa877acd2c12ce7027e9048f20e
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-24 15:22:55 +0200

    Finished error css

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styles from './error.module.css'
 
 const Error = () => {
     return (
         <>
-            <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <p><Link to='/home'>Retourner sur la page d’accueil</Link></p>
+            <h1 className={styles.title}>404</h1>
+            <p className={styles.text}>Oups! La page que vous demandez n'existe pas.</p>
+            <p><Link to='/home' className={styles.link}>Retourner sur la page d’accueil</Link></p>
         </>
     );
 };
