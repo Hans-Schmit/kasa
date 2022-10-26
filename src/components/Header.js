e62a4442cda1f714d7849ee5c311a814bc561ed0
Hans Schmit <hans.schmit@hotmail.fr>
2022-10-26 14:25:12 +0200

    Added active page link underline

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom'
+import { pageService } from '../_services/Page.service'
 import styles from './header.module.css'
 import logo1 from '../images/logo_1.png'
 
 const Header = () => {
+
+    const [linkHome, setLinkHome] = useState(styles.link)
+    const [linkAPropos, setLinkAPropos] = useState(styles.link)
+
+    const flagLinks = useRef(false)
+
+    const switchActive = (link) => {
+        link === 'home' ? setLinkHome(styles.linkActive) : setLinkHome(styles.link)
+        link === 'apropos' ? setLinkAPropos(styles.linkActive) : setLinkAPropos(styles.link)
+    }
+
+
+    useEffect(() => {
+        if (flagLinks.current === false) {
+            if (pageService.isHome()) {
+                setLinkHome(styles.linkActive)
+            }
+            if (pageService.isAPropos()) {
+                setLinkAPropos(styles.linkActive)
+            }
+        }
+
+        return () => flagLinks.current = true
+    }, [])
+
     return (
         <header className={styles.header}>
             <img src={logo1} alt='Logo Kasa' className={styles.logo} />
             <nav className={styles.nav}>
-                <Link to='/home' className={styles.link}>Accueil</Link>
-                <Link to='/a-propos' className={styles.link}>A Propos</Link>
+                <Link to='/home' className={linkHome} onClick={() => switchActive("home")}>Accueil</Link>
+                <Link to='/a-propos' className={linkAPropos} onClick={() => switchActive("apropos")}>A Propos</Link>
             </nav>
         </header>
     );
